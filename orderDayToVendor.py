20f4bec4b76b3673f8b72ed4b6a25370460e1653
Ashutosh Local <Ashutosh Local>
2016-06-06 14:46:19 -0400

    Adding order day calc algo and a microservice to host the service

---
+++ orderDayToVendor.py
@@ -0,0 +1,29 @@
+
+def calculate_order_day_to_vendor(input_list):
+    sample = [0,0,1,0,0,0,1]
+    index = 0
+    day_count = 0
+    curr_1_index = -1
+    loop_count = 1
+    while input_list.count(1) > 0 and loop_count <= (len(input_list) * 2 + 1):
+        try:
+            if input_list[index] >= 1:
+                if curr_1_index < 0:
+                    # for the first non zero value, do not update the array
+                    curr_1_index = index
+                    day_count = 1
+                else:
+                    # Update the array and reset counters
+                    input_list[curr_1_index] = day_count
+                    day_count = 1
+                    curr_1_index = index
+            elif input_list[index] == 0 and curr_1_index >= 0:
+                day_count += 1
+            index += 1
+            loop_count += 1
+        except IndexError:
+            index = 0
+            loop_count += 1
+
+    print(input_list)
+    return input_list
---
+++ pythontestmicroservice.py
@@ -0,0 +1,25 @@
+#import ibm_db as db2
+import json
+import os
+from flask import Flask, jsonify, make_response, request
+
+import orderDayToVendor as odv
+
+app = Flask(__name__)
+
+@app.route('/posthello', methods = ['POST'])
+def hello_world():
+  return 'Hi There!!' 
+
+@app.route('/<string:input_list>/orderdaytovendor', methods = ['GET'])
+def order_day_to_vendor(input_list):
+	# Split the comma seperated string values into a list 
+	input_list = input_list.split(',')
+	# Convert the string list into int list
+	input_list = [int(i) for i in input_list]
+	return str(odv.calculate_order_day_to_vendor(input_list))
+
+if __name__ == '__main__':
+  app.debug = True
+  PORT = int(os.environ.get('PORT'))
+  app.run(host = '0.0.0.0', port = PORT)
